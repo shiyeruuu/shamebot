ddc5035a5e35f5c0c74c18d3fb11a15bafe46f46
shiyeruuu <abthnya01@gmail.com>
2026-05-26 12:26:00 -0600

    image

--- discordBot.py
+++ discordBot.py
@@ -26,7 +26,7 @@ async def on_voice_state_update(member, before, after):
 
 	guild = member.guild
 
-	general = discord.utils.get(guild.text_channels, name='┊・⊱・shining-shame-ring')
+	general = discord.utils.get(guild.text_channels, name='┊・⊱・wall-of-shame')
 	if not general:
 		return
 
@@ -43,13 +43,9 @@ async def on_voice_state_update(member, before, after):
 	if disconnector.id == 902424435489923102:
 		return
 	
-	await general.send(
-			f'**{member.display_name}** was disconnected from '
-			f'**{before.channel.name}** by **{disconnector.display_name} **.'
-			f'**SHAME ON YOU!!!"**'
-		)
-	return
-
+	embed = discord.Embed(title="Member Disconnected", description=f"{member.mention} was disconnected from {before.channel.mention} by {disconnector.mention} SHAME ON YOU!")
+	embed.set_image(url='https://imgur.com/a/jBctAAF')
+	await general.send(embed=embed)
 
 client.run(os.getenv('DISCORD_TOKEN'))
 
